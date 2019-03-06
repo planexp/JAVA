2e4b81cba84cf9d36a360653ce0e8345e6e96dc1
Marcin Snarski <company.m2s@gmail.com>
2019-03-06 09:42:36 +0100

    First Commit

---
+++ src/main/java/com/kodilla/FirstClass.java
@@ -0,0 +1,20 @@
+package com.kodilla;
+
+
+class Kodilla {
+    public static void main(String[] args) {
+        // your code goes here
+        {
+            boolean isGoodWeather = true;
+
+            if(isGoodWeather) {
+
+                System.out.println("Jupi!");
+
+            } else {
+
+                System.out.println("Blee");
+            }
+        }
+    }
+}
