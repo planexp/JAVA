d59f8478d0c4e92974508b8a09f2bace3a8b34bd
Marcin Snarski <company.m2s@gmail.com>
2019-03-06 12:11:55 +0100

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 class Calculator {
-    public void someMethod(){
-
+    public void someMethod() {
     }
-
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.someMethod();
 
-        int inInt1;
-        int inInt2;
-        String result;
+                double inInt1;
+                double inInt2;
+                String result;
 
-        Scanner in = new Scanner(System.in);
+                Scanner in = new Scanner(System.in);
 
-        System.out.println("please enter the first number:");
-        inInt1 = in.nextInt();
+                System.out.println("please enter the first number:");
+                inInt1 = in.nextDouble();
 
-        System.out.println("please enter the second number:");
-        inInt2 = in.nextInt();
+                System.out.println("please enter the second number:");
+                inInt2 = in.nextDouble();
 
-        Scanner re = new Scanner(System.in);
-        System.out.println("Please enter operation: +  or - ");
-        result = re.next();
+                Scanner re = new Scanner(System.in);
+                System.out.println("Please enter operation: + or - ");
+                result = re.next();
 
-        if (result.equals("+")) {
-            System.out.println("your answer is = " + (inInt1 + inInt2));
-        }else if (result.equals("-")) {
-            System.out.println("your answer is = " + (inInt1 + inInt2));
+                if (result.equals("+")) {
+                    System.out.println("your answer is = " + (inInt1 + inInt2));
+                } else if (result.equals("-")) {
+                    System.out.println("your answer is = " + (inInt1 + inInt2));
+                }else {
+                    System.out.println("!!! Add correct parameter '+' or '-' !!!");
 
         }
     }
