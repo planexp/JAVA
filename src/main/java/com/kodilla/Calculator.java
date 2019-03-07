761caf77189c5f883b2ec78af0d211d68c6e3746
Marcin Snarski <company.m2s@gmail.com>
2019-03-07 07:35:42 +0100

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,35 +1,45 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Calculator {
     public void someMethod() {
+
     }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.someMethod();
 
-                double inInt1;
-                double inInt2;
-                String result;
+        double inInt1;
+        double inInt2;
+        String result;
 
-                Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-                System.out.println("please enter the first number:");
-                inInt1 = in.nextDouble();
+        System.out.println("please enter the first number:");
+        inInt1 = in.nextDouble();
 
-                System.out.println("please enter the second number:");
-                inInt2 = in.nextDouble();
+        System.out.println("please enter the second number:");
+        inInt2 = in.nextDouble();
 
-                Scanner re = new Scanner(System.in);
-                System.out.println("Please enter operation: + or - ");
-                result = re.next();
+        result = dzialanie();
 
-                if (result.equals("+")) {
-                    System.out.println("your answer is = " + (inInt1 + inInt2));
-                } else if (result.equals("-")) {
-                    System.out.println("your answer is = " + (inInt1 + inInt2));
-                }else {
-                    System.out.println("!!! Add correct parameter '+' or '-' !!!");
+        if (result.equals("+")) {
+            System.out.println("your answer is = " + (inInt1 + inInt2));
+        } else if (result.equals("-")) {
+            System.out.println("your answer is = " + (inInt1 - inInt2));
 
+        }
+    }
+
+    public static String dzialanie() {
+        Scanner re = new Scanner(System.in);
+        System.out.println("Please enter operation: + or - ");
+        String result = re.next();
+
+        if (result.equals("+") || result.equals("-")) {
+            return result;
+        } else {
+            return dzialanie();
         }
     }
 }
---
+++ src/main/java/com/kodilla/Temp.java
@@ -0,0 +1,23 @@
+package com.kodilla;
+
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        int tem= 0;
+
+        Scanner moj = new Scanner(System.in);
+
+        System.out.println("Podaj temperature");
+
+       tem = moj.nextInt();
+        if (tem >=1) {
+            System.out.println(" Temp. dodatnia");
+        }else  if (tem == 0){
+            System.out.println("Temp. jest równa 0");
+        }else {
+            System.out.println(" Temp. ujemna");
+        }
+
+    }
+}
