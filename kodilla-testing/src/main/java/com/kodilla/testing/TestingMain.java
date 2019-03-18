67ce20b0309b91a7a6ec0a3f246480342b279614
Marcin Snarski <company.m2s@gmail.com>
2019-03-18 19:21:21 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,24 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
+        Calculator obiect = new Calculator();
 
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int result = obiect.add(5,2);
+        if (result == 7){
+            System.out.println("test1  OK");
+        } else {
+            System.out.println("Error test1 !");
+
+            int result2 = obiect.subtract(5, 2);
+            if (result2 == 3) {
+                System.out.println("test 2 OK");
+            } else {
+                System.out.println("Error test2!");
+
+            }
+        }
+
     }
 }
